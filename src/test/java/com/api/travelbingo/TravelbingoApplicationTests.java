dacca71dfc3fadf811eefdc0f7fbe14a3fe7d2a9
liamskydamien <liamskydamien@googlemail.com>
2022-11-11 16:07:34 +0100

    Initial commit

---
+++ src/test/java/com/api/travelbingo/TravelbingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.travelbingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
