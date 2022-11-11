609c6ca9cf6bd275448ba16784167c071dc383e9
liamskydamien <liamskydamien@googlemail.com>
2022-11-11 18:40:06 +0100

    Adding user entity

---
+++ client/src/main/java/com/travelbingo/client/entities/User.java
@@ -0,0 +1,46 @@
+package com.travelbingo.client.entities;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+
+    private String userName;
+
+    private String userEmail;
+
+    @Column(length = 60)
+    private String userPassword;
+
+    private String userFirstName;
+
+    private String userLastname;
+
+    private String userRole;
+
+    private boolean isEnabled;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return userID != null && Objects.equals(userID, user.userID);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
