0e34766ba7a03f6dd9363adc198cdb98a00109a6
liamskydamien <liamskydamien@googlemail.com>
2022-11-12 14:35:06 +0100

    Add Ressource Server

--- client/src/main/java/com/travelbingo/client/entities/User.java
+++ client/src/main/java/com/travelbingo/client/entities/User.java
@@ -15,7 +15,6 @@ import java.util.Objects;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long userID;
 
     private String userName;
