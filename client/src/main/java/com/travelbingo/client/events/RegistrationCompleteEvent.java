1accbc208f031a4737c81ccb77e0e6365d8516ce
liamskydamien <liamskydamien@googlemail.com>
2022-11-12 13:08:49 +0100

    Adding Eventhandler

---
+++ client/src/main/java/com/travelbingo/client/events/RegistrationCompleteEvent.java
@@ -0,0 +1,20 @@
+package com.travelbingo.client.events;
+
+import com.travelbingo.client.entities.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class RegistrationCompleteEvent extends ApplicationEvent {
+
+    private final User user;
+    private final String applicationUrl;
+
+    public RegistrationCompleteEvent(User user, String applicationUrl) {
+        super(user);
+        this.user = user;
+        this.applicationUrl = applicationUrl;
+    }
+}
