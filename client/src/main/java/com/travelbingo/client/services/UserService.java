44676281ee78194468018c7e321e03873eaa0287
liamskydamien <liamskydamien@googlemail.com>
2022-11-12 13:05:40 +0100

    Adding more needed Configuration

---
+++ client/src/main/java/com/travelbingo/client/services/UserService.java
@@ -0,0 +1,119 @@
+package com.travelbingo.client.services;
+
+import com.travelbingo.client.entities.User;
+import com.travelbingo.client.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService    {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
+
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(UserModel userModel) {
+        User user = new User();
+        user.setEmail(userModel.getEmail());
+        user.setFirstName(userModel.getFirstName());
+        user.setLastName(userModel.getLastName());
+        user.setRole("USER");
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
+
+        userRepository.save(user);
+        return user;
+    }
+
+    public void saveVerificationTokenForUser(String token, User user) {
+        VerificationToken verificationToken
+                = new VerificationToken(user, token);
+
+        verificationTokenRepository.save(verificationToken);
+    }
+
+    public String validateVerificationToken(String token) {
+        VerificationToken verificationToken
+                = verificationTokenRepository.findByToken(token);
+
+        if (verificationToken == null) {
+            return "invalid";
+        }
+
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if ((verificationToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
+            verificationTokenRepository.delete(verificationToken);
+            return "expired";
+        }
+
+        user.setEnabled(true);
+        userRepository.save(user);
+        return "valid";
+    }
+
+    public VerificationToken generateNewVerificationToken(String oldToken) {
+        VerificationToken verificationToken
+                = verificationTokenRepository.findByToken(oldToken);
+        verificationToken.setToken(UUID.randomUUID().toString());
+        verificationTokenRepository.save(verificationToken);
+        return verificationToken;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void createPasswordResetTokenForUser(User user, String token) {
+        PasswordResetToken passwordResetToken
+                = new PasswordResetToken(user,token);
+        passwordResetTokenRepository.save(passwordResetToken);
+    }
+
+    public String validatePasswordResetToken(String token) {
+        PasswordResetToken passwordResetToken
+                = passwordResetTokenRepository.findByToken(token);
+
+        if (passwordResetToken == null) {
+            return "invalid";
+        }
+
+        User user = passwordResetToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if ((passwordResetToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
+            passwordResetTokenRepository.delete(passwordResetToken);
+            return "expired";
+        }
+
+        return "valid";
+    }
+
+    public Optional<User> getUserByPasswordResetToken(String token) {
+        return Optional.ofNullable(passwordResetTokenRepository.findByToken(token).getUser());
+    }
+
+    public void changePassword(User user, String newPassword) {
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+    }
+
+    public boolean checkIfValidOldPassword(User user, String oldPassword) {
+        return passwordEncoder.matches(oldPassword, user.getPassword());
+    }
+}
