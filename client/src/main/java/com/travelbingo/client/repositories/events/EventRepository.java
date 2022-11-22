83b062247f5402ef456c1a205c0fa44534174bad
liamskydamien <liamskydamien@gmail.com>
2022-11-22 19:48:13 +0100

    Adding functions

--- client/src/main/java/com/travelbingo/client/repositories/events/EventRepository.java
+++ client/src/main/java/com/travelbingo/client/repositories/events/EventRepository.java
@@ -1,6 +1,7 @@
 package com.travelbingo.client.repositories.events;
 
 import com.travelbingo.client.entities.events.Category;
+import com.travelbingo.client.entities.events.Country;
 import com.travelbingo.client.entities.events.Event;
 import com.travelbingo.client.entities.events.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +14,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     public List<Event> findEventsByCategory(Category category);
     public List<Event> findEventsByLocation(Location location);
 
-    public List<Event> findEventsByLocation_Country(Location location);
+    public List<Event> findEventsByLocation_Country(Country location_country);
 
 }
--- client/src/main/java/com/travelbingo/client/services/EventService.java
+++ client/src/main/java/com/travelbingo/client/services/EventService.java
@@ -1,6 +1,9 @@
 package com.travelbingo.client.services;
 
+import com.travelbingo.client.entities.events.Category;
+import com.travelbingo.client.entities.events.Country;
 import com.travelbingo.client.entities.events.Event;
+import com.travelbingo.client.entities.events.Location;
 import com.travelbingo.client.models.interfaces.BingoModel;
 import com.travelbingo.client.repositories.events.EventRepository;
 import com.travelbingo.client.services.interfaces.ServiceInterface;
@@ -24,6 +27,18 @@ public class EventService implements ServiceInterface<Event> {
     @Override
     public Event findByName(String name) {
         return eventRepository.findEventByName(name).orElseThrow();
+    }
+
+    public List<Event> findByCategory(Category category){
+        return eventRepository.findEventsByCategory(category);
+    }
+
+    public List<Event> findByLocation(Location location){
+        return eventRepository.findEventsByLocation(location);
+    }
+
+    public List<Event> findByCountry(Country country){
+        return eventRepository.findEventsByLocation_Country(country);
     }
 
     @Override
